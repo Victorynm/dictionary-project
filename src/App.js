ba3e562461d0eb9bf96ccffded7a7c2edc502cb9
Victorynm <victorynm22@gmail.com>
2022-09-13 16:20:03 +0300

    Added search form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">Hello world</header>
+		<div className="App text-center">
+			<header className="App-header"></header>
+			<h1>Dictionary</h1>
+			<h2>What word do you want to look up?</h2>
+			<form>
+				<input type="search" />
+				<input type="submit" value="Search" />
+			</form>
 		</div>
 	);
 }
