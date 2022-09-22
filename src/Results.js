fc613ec08ebb5dd4acc732649a07d0c32e24f07d
Victorynm <victorynm22@gmail.com>
2022-09-22 09:31:29 +0300

    add audio

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Meaning from "./Meaning";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Results(props) {
+	console.log(props.results);
 	if (props.results) {
 		return (
 			<div className="Results">
 				<h2>{props.results.word}</h2>
 				<h3 className="text-muted">{props.results.phonetic}</h3>
+				<ReactAudioPlayer src={props.results.phonetics[0].audio} controls />
 				{props.results.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
